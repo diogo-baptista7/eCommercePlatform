b3efbbe182f646bf519f0db68fc21e6bdc8b6bf5
diogo-baptista7 <mab4.diogo@gmail.com>
2023-08-14 01:22:32 +0100

    Added way to show products and also a Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -9,8 +10,8 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Mundial</h1>
-          </Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </>
